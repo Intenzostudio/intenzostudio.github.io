9d5504b7f628616ee9c421ea78df32cb21ed2e2e
Intenzostudio <34794349+Intenzostudio@users.noreply.github.com>
2018-02-24 23:59:59 +0300

    45

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
 		// Map marker 
 		var image = '/assets/images/map-pin.png';
-		var myLatLng = new google.maps.LatLng(26.2385, 50.5960);
+		var myLatLng = new google.maps.LatLng(26.221843, 50.610615);
 		var beachMarker = new google.maps.Marker({
 			position: myLatLng,
 			map: map,
